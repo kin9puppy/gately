382e37d91b851c0d36c2b26d5d57ff05f7a386fe
KingPuppy <michaeljlhuey@gmail.com>
2015-07-21 17:27:02 -0500

    build

--- Gately/LL/GetFollowers/GetFollowers.cs
+++ Gately/LL/GetFollowers/GetFollowers.cs
@@ -21,8 +21,8 @@ namespace Gately.LL.GetFollowers
         }
 
         public bool get() {
-
-            var California = new string[] {  "California_Institute_of_Technology", "Stanford_University", "University_of_California_Berkeley", "University_of_California_Davis", "University_of_California_Irvine",
+            //"California_Institute_of_Technology", 
+            var California = new string[] {  "Stanford_University", "University_of_California_Berkeley", "University_of_California_Davis", "University_of_California_Irvine",
                 "University_of_California_Los_Angeles", "University_of_California_Merced", "University_of_California_Riverside", "University_of_California_San_Diego2", "University_of_California_San_Francisco", 
                 "University_of_California_Santa_Barbara", "University_of_California_Santa_Cruz","University_of_Southern_California","California_State_University_Long_Beach","California_State_University_Los_Angeles",
                 "California_State_University_Northridge","San_Jose_State_University","San_Diego_State_University", "University_of_San_Francisco", "Sonoma_State_University", "Chapman_University", "Harvey_Mudd_College", 
@@ -32,7 +32,7 @@ namespace Gately.LL.GetFollowers
 
             try
             {
-                
+                var userCount   = 0;
                 var worker      = new lib.worker(dataFolder);
                 var ser         =  new JavaScriptSerializer();
                 dynamic user;
@@ -51,9 +51,12 @@ namespace Gately.LL.GetFollowers
                             user = ser.Deserialize<User>(line);
                             worker.saveUser(user);
                             count++;
-                            
+                            userCount++;
                             Console.WriteLine(user.name + " added [" + user.org + "]"); 
                             Console.WriteLine(count + " contacts in " + stopwatch.ElapsedMilliseconds / 1000 + " sec");
+                            if (userCount == 1000) {
+                                userCount = 0;
+                            }
                         }
                         catch { }
                     }
